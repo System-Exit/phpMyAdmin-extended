0a725cef7fe077f74fe54799dbf8fd8a17fc0d46
Daniel Linford <s3661720@student.rmit.edu.au>
2018-08-11 19:38:22 +1000

    Got query working.
    Connects to database as control user and performs a working query.

--- libraries/classes/Controllers/Giganibbles/ServerMessagesController.php
+++ libraries/classes/Controllers/Giganibbles/ServerMessagesController.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace PhpMyAdmin\Controllers\Giganibbles;
 
 use PhpMyAdmin\Controllers\Controller;
+use PhpMyAdmin\DatabaseInterface;
 use PhpMyAdmin\Relation;
 use PhpMyAdmin\Response;
 
@@ -22,38 +23,36 @@ use PhpMyAdmin\Response;
  */
 class ServerMessagesController extends Controller
 {
-
-    protected $relation;
-
     protected $user;
-
+    protected $query;
     protected $result;
+
+    protected $sender;
+    protected $timestamp;
+    protected $message;
 
     public function indexAction()
     {
         global $cfg;
-        // Connect to sql
-        $relation = new Relation();
+        // Get access to database
+        /*DatabaseInterface::load(); (This line might be needed?)*/
+        $this->dbi->connect(DatabaseInterface::CONNECT_CONTROL);
+        $this->dbi->selectDb('phpmyadmin');
         // Get current user
         $user = $cfg['Server']['user'];
         // Get messages from database
-        $result = $relation->queryAsControlUser("SELECT `message` FROM `pma__messages` WHERE `receiver` LIKE `$user`");
-
+        $query = "SELECT `msg`.`receiver` FROM `pma__messages` `msg` WHERE `msg`.`receiver` LIKE '$user'";
+        $result = $this->dbi->query($query);
+        
         $response = Response::getInstance();
 
         $response->addHTML($this->template->render(
             'Giganibbles/server_messages',
             [
                 // vars go here!
-                'result' => $this->result
+                'result' => $result->num_rows
             ]
         ));
-
-         return $result->num_rows;
     }
 
 }
-
-$c = new ServerMessagesController();
-$r = $c->indexAction();
-echo $r;
