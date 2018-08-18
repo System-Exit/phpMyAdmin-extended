955d531cfaf894ebbd8969dbca89b9dd42257c92
Daniel Linford <s3661720@student.rmit.edu.au>
2018-08-18 14:36:20 +1000

    Added basic test for page merge
    Created a working Selenium test for the page merge. Checks if the user account contains both the users list and user groups list.

---
+++ test/selenium/giganibbles/UserAccountsPageTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rocky
+ * Date: 8/18/2018
+ * Time: 11:33 AM
+ */
+
+namespace PhpMyAdmin\Tests\Selenium;
+
+
+class UserAccountsPageTest extends TestBase
+{
+    protected function setup()
+    {
+        parent::setUp();
+    }
+
+    public function setUpPage()
+    {
+        parent::setUpPage();
+        $this->login();
+    }
+
+    public function testMerge()
+    {
+        $link = $this->waitForElement('byXPath', "//a[contains(@href, 'server_privileges.php')]");
+        $link->click();
+        $link->click();
+        $this->waitAjax();
+
+        $this->assertTrue($this->isElementPresent('byId', "usersForm"));
+        $this->assertTrue($this->isElementPresent('byId', "userGroupsForm"));
+    }
+}
