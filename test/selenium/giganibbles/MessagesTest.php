0725f20ee54f10eed026c389947ac4e754d1301d
Daniel Linford <s3661720@student.rmit.edu.au>
2018-09-05 17:24:29 +1000

    Updated message page tests
    Updated tests to work with updates made to page and added some extra tests, including a test for the 'NEW' indicator in unseen messages.

--- test/selenium/giganibbles/MessagesTest.php
+++ test/selenium/giganibbles/MessagesTest.php
@@ -40,7 +40,6 @@ class MessagesTest extends TestBase
     protected function setup()
     {
         parent::setUp();
-
         $this->sender = $GLOBALS['TESTSUITE_USER'];
         $this->receiver = $GLOBALS['TESTSUITE_USER'];
         $this->message = "Selenium test message";
@@ -52,18 +51,20 @@ class MessagesTest extends TestBase
         $this->login();
     }
 
+    /**
+     * Tests message sending functionality for sending a message to oneself
+     *
+     * Tests:
+     * - if sent message is present in message database
+     * - if new message is visible on loaded page
+     */
     public function testSendMessage()
     {
-        // Go to messages page
-        $this->expandMore();
-        $this->waitForElement('byXPath', "//a[contains(@href, 'server_messages.php')]")->click();
-        $this->waitAjax();
-
-        //Let page load
-        $this->waitAjax();
+        // Goto messages page
+        $this->goToMessagesPage();
 
         // Add message to send
-        $this->scrollIntoView('form_message');
+        //$this->scrollIntoView('form_message');
         $ele = $this->waitForElement("byId", "form_message");
         $this->moveto($ele);
         $ele->click();
@@ -73,7 +74,7 @@ class MessagesTest extends TestBase
         $messageField->value($this->message);
 
         // Add receiver to receive message
-        $this->scrollIntoView('form_receiver');
+        //$this->scrollIntoView('form_receiver');
         $ele = $this->waitForElement("byId", "form_receiver");
         $this->moveto($ele);
         $ele->click();
@@ -83,7 +84,7 @@ class MessagesTest extends TestBase
         $receiverField->value($this->receiver);
 
         // Attempt to send message
-        $sendButton = $this->waitForElement("byXPath", "//input[@value='Send']");
+        $sendButton = $this->waitForElement("byId", "message_form_send");
         $this->moveto($sendButton);
         $sendButton->click();
 
@@ -99,8 +100,73 @@ class MessagesTest extends TestBase
         $this->refresh();
 
         //Check if new message is visible
+        $this->waitForElement('byXPath', "//li[@class='message_body']");
         $this->assertTrue($this->isElementPresent('byXPath', "//p[text()='$this->message']"));
 
+        // Delete test message
+        $this->deleteTestMessages();
+    }
+
+    /**
+     * Tests message receiving
+     *
+     * Tests:
+     * - Created message is visible on messages page
+     * - Message is marked as new when first loaded
+     * - Message is not marked when loaded again
+     */
+    public function testMessageReceive()
+    {
+        // Load messages page to clear any new messages
+        $this->goToMessagesPage();
+
+        // Creates a new message directly
+        $query = "INSERT INTO phpmyadmin.pma__messages "
+            . "(`sender`, `receiver`, `timestamp`, `message`, `seen`) "
+            . "VALUES ('$this->sender', '$this->receiver', utc_timestamp(), '$this->message', 0);";
+        $this->dbQuery($query);
+
+        // Reload messages page
+        $this->refresh();
+
+        // Check if new message is present
+        $this->waitForElement('byXPath', "//li[@class='message_body']");
+        $this->assertTrue($this->isElementPresent('byXPath',  "//p[text()='$this->message']"));
+
+        // Check if message is marked as new
+        $this->assertTrue($this->isElementPresent('byXPath',
+            "//li[p[text()='$this->message'] and p[@class='message_new']]"));
+
+        // Reload messages page
+        $this->refresh();
+
+        // Check that the message is no longer marked as new
+        $this->waitForElement('byXPath', "//li[@class='message_body']");
+        $this->assertFalse($this->isElementPresent('byXPath',
+            "//li[p[text()='$this->message'] and p[@class='message_new']]"));
+
+        // Delete test message
+        $this->deleteTestMessages();
+    }
+
+    /**
+     * Helper function to go directly to messages page
+     */
+    private function goToMessagesPage()
+    {
+        // Go to messages page
+        $this->expandMore();
+        $this->waitForElement('byXPath', "//a[contains(@href, 'server_messages.php')]")->click();
+
+        //Let page load
+        $this->waitAjax();
+    }
+
+    /**
+     * Helper function to remove all created test messages
+     */
+    private function deleteTestMessages()
+    {
         // Delete test message from database
         $query = "DELETE FROM `phpmyadmin`.`pma__messages` WHERE message = '$this->message';";
         $this->dbQuery($query);
