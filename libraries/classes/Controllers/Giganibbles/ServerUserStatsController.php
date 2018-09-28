bc069c9c28e2c4c7bb5c9c41fafa30a8462c740d
Daniel Linford <s3661720@student.rmit.edu.au>
2018-09-28 17:32:58 +1000

    User stats added
    Added code to get user's username, server, user group and tabs the user group is allowed to view.

--- libraries/classes/Controllers/Giganibbles/ServerUserStatsController.php
+++ libraries/classes/Controllers/Giganibbles/ServerUserStatsController.php
@@ -31,14 +31,23 @@ class ServerUserStatsController extends Controller
      */
     public function indexAction()
     {
+        global $cfg;
+        $relation = new Relation();
 
-        // get all necessary data
+        // Get user information
         $info_list = [
-            'name'      => 'None',
+            'name'      => $cfg['Server']['user'],
+            // TODO: Get date of user creation
             'date'      => 'None',
-            'server'    => 'None'
+            'server'    => $cfg['Server']['host']
         ];
-        $permissions = ['pma' => [], 'mysql' => []];
+        // Get user permissions
+        //$query = "SELECT mysql.db";
+        //$rawPermissions;
+        $permissions = [
+            'pma'       => $this->getUserPmaPrivs($cfg['Server']['user']),
+            'mysql'     => []
+        ];
         $usage_list = [
             ['type' => 'None', 'value' => 'None']
         ];
@@ -67,12 +76,78 @@ class ServerUserStatsController extends Controller
     /**
      * Accesses user permissions from the PMA usergroups list. This data is
      * appended to the permissions list.
+     * @param string $user username of user to get user group permissions from
      * @return array An array of all values to be added. All values are returned
      *               as key => value pairs, where the column name is the key and
      *               the column value is the value.
      */
-    private function getUserPmaPrivs() : array {
-        return null;
+    private function getUserPmaPrivs($user) : array {
+        $relation = new Relation();
+
+        // Get user group of user
+        $usergroup = $this->getUserGroupOfUser($user);
+        // If the user doesn't have a user group, indicate this in a single value in array
+        if(empty($usergroup))
+        {
+            return ["User has no user group and is not restricted by tab view."];
+        }
+        // Start string array to return with name of user group
+        $privilegesStrings = ["User group: $usergroup"];
+        // Get the permissions for the user group
+        $query = "SELECT * "
+            . "FROM phpmyadmin.pma__usergroups "
+            . "WHERE usergroup = '$usergroup'";
+        $result = $relation->queryAsControlUser($query);
+        // Parse through server, database and table level tab permissions
+        $privileges = [];
+        while($row = $result->fetch_assoc())
+        {
+            // Skip row if privilege is not allowed
+            if($row['allowed'] == 'N') continue;
+
+            // Determine privilege category and store it in privileges array
+            if(strpos($row['tab'], "server_") !== false)
+                $privileges['Server'][] = explode('server_', $row['tab'])[1];
+            if(strpos($row['tab'], "db_") !== false)
+                $privileges['Database'][] = explode('db_', $row['tab'])[1];
+            if(strpos($row['tab'], "table_") !== false)
+                $privileges['Table'][] = explode('table_', $row['tab'])[1];
+        }
+        // Build strings for each category that has a valid permission and adds it to privileges strings
+        // Each privilege has all '_' replaces with spaces for readability and is separated by commas
+        if(isset($privileges['Server']))
+        {
+            $catToAdd = "Server: ";
+            for($i = 0; $i < count($privileges['Server']); $i++)
+            {
+                $catToAdd .= str_replace("_", " ", $privileges['Server'][$i]).", ";
+            }
+            $catToAdd = rtrim($catToAdd, ", ");
+            $privilegesStrings[] = $catToAdd;
+        }
+        if(isset($privileges['Database']))
+        {
+            $catToAdd = "Database: ";
+            for($i = 0; $i < count($privileges['Database']); $i++)
+            {
+                $catToAdd .= str_replace("_", " ", $privileges['Database'][$i]).", ";
+            }
+            $catToAdd = rtrim($catToAdd, ", ");
+            $privilegesStrings[] = $catToAdd;
+        }
+        if(isset($privileges['Table']))
+        {
+            $catToAdd = "Table: ";
+            for($i = 0; $i < count($privileges['Table']); $i++)
+            {
+                $catToAdd .= str_replace("_", " ", $privileges['Table'][$i]).", ";
+            }
+            $catToAdd = rtrim($catToAdd, ", ");
+            $privilegesStrings[] = $catToAdd;
+        }
+
+        // Return all valid built strings for privileges
+        return $privilegesStrings;
     }
 
     /**
@@ -80,8 +155,18 @@ class ServerUserStatsController extends Controller
      * @return mixed A usergroup for the current user, or null if the user has
      *               not been assigned a usergroup.
      */
-    private function getUserGroupOfUser() {
-        return null;
+    private function getUserGroupOfUser($user){
+        $relation = new Relation();
+
+        // Query for user group of user
+        $query = "SELECT username, usergroup "
+            . "FROM phpmyadmin.pma__users "
+            . "WHERE username = '$user'; ";
+        $result = $relation->queryAsControlUser($query);
+        $row = $result->fetch_assoc();
+
+        // Return user group
+        return $row['usergroup'];
     }
 
     /**
